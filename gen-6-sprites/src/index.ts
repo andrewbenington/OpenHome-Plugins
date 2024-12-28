8d0e60ae7c7d971db267e757aa8129b647ffa67f
Andrew Benington <andrewbenington57@gmail.com>
2024-12-27 21:59:32 -0600

    gen 6 plugin

---
+++ gen-6-sprites/src/index.ts
@@ -0,0 +1,36 @@
+import { NationalDex, PokemonData } from "pokemon-species-data";
+
+interface MonData {
+  dexNum: number;
+  formeNum: number;
+  format: string;
+  isFemale?: boolean;
+  isShiny?: boolean;
+}
+
+interface SpritePlugin {
+  pluginName: string;
+  pluginID: string;
+  getMonSpritePath: (params: MonData) => string | null;
+}
+
+export const plugin: SpritePlugin = {
+  pluginName: "Gen 6 Sprites",
+  pluginID: "gen-6-sprites",
+  getMonSpritePath: (params: MonData) => {
+    const { dexNum, formeNum, format, isShiny } = params;
+
+    if (format !== "PK6") return null;
+    if (dexNum > NationalDex.Volcanion) return null;
+
+    const speciesData = PokemonData[dexNum];
+    if (!speciesData) return null;
+
+    const formeData = speciesData.formes[formeNum];
+    if (!formeData || formeData.gen > 6) return null;
+
+    if (formeData.regional) return null;
+
+    return `assets/${isShiny ? "shiny/" : ""}${formeData.sprite}.png`;
+  },
+};
